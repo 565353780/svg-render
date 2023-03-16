851eafe53c2f9e5f400119a0d11054e87273bc19
chLi <565353780@qq.com>
2023-03-17 02:53:03 +0800

    finish auto scale data into image

--- svg_render/Module/renderer.py
+++ svg_render/Module/renderer.py
@@ -18,6 +18,11 @@ class Renderer(object):
         self.height = height
         self.free_width = free_width
 
+        self.x_min = float('inf')
+        self.y_min = float('inf')
+        self.x_max = -float('inf')
+        self.y_max = -float('inf')
+
         self.origin_translate = None
         self.scale = None
         self.post_translate = np.array([self.width / 2.0, self.height / 2.0],
@@ -27,6 +32,11 @@ class Renderer(object):
         return
 
     def reset(self):
+        self.x_min = float('inf')
+        self.y_min = float('inf')
+        self.x_max = -float('inf')
+        self.y_max = -float('inf')
+
         self.origin_translate = None
         self.scale = None
 
@@ -34,21 +44,77 @@ class Renderer(object):
         return True
 
     def resetImage(self):
-        self.image = np.zeros([self.width, self.height, 3], dtype=np.uint8)
+        self.image = np.zeros([self.height, self.width, 3], dtype=np.uint8)
         return True
 
-    def updateTransform(self, view_box_start, view_box_size):
+    def addPoint(self, point):
+        self.x_min = min(self.x_min, point[0])
+        self.y_min = min(self.y_min, point[1])
+        self.x_max = max(self.x_max, point[0])
+        self.y_max = max(self.y_max, point[1])
+        return True
+
+    def updateViewBox(self, svg_data):
+        unknown_dtype_list = []
+
+        for segment, dtype in zip(svg_data['segment_list'],
+                                  svg_data['dtype_list']):
+            if dtype == 'Line':
+                start = [segment.start.real, segment.start.imag]
+                end = [segment.end.real, segment.end.imag]
+
+                self.addPoint(start)
+                self.addPoint(end)
+            elif dtype == 'Arc':
+                #  start = [float(segment.start.real), float(segment.start.imag)]
+                #  end = [float(segment.end.real), float(segment.end.imag)]
+                #  rotation = float(segment.rotation)
+                #  large_arc = segment.large_arc
+                #  sweep = segment.sweep
+
+                pixel_width = int(max(segment.radius.real,
+                                      segment.radius.imag)) + 2
+
+                for i in range(pixel_width + 1):
+                    point = segment.point(1.0 * i / pixel_width)
+
+                    self.addPoint([point.real, point.imag])
+            elif dtype == 'Circle':
+                cx = float(segment.attrib['cx'])
+                cy = float(segment.attrib['cy'])
+                r = float(segment.attrib['r'])
+
+                self.addPoint([cx - r, cy - r])
+                self.addPoint([cx + r, cy + r])
+            elif dtype == 'Ellipse':
+                cx = float(segment.attrib['cx'])
+                cy = float(segment.attrib['cy'])
+                rx = float(segment.attrib['rx'])
+                ry = float(segment.attrib['ry'])
+
+                self.addPoint([cx - rx, cy - ry])
+                self.addPoint([cx + rx, cy + ry])
+            else:
+                if dtype not in unknown_dtype_list:
+                    print("[WARN][Renderer::updateViewBox]")
+                    print("\t can not solve this segment with type [" + dtype +
+                          "]!")
+                    unknown_dtype_list.append(dtype)
+        return True
+
+    def updateTransform(self, svg_data):
         self.reset()
 
+        self.updateViewBox(svg_data)
+
         self.origin_translate = np.array([
-            -view_box_start[0] - view_box_size[0] / 2.0,
-            -view_box_start[1] - view_box_size[1] / 2.0
+            -(self.x_min + self.x_max) / 2.0, -(self.y_min + self.y_max) / 2.0
         ],
                                          dtype=float)
 
         self.scale = min(
-            (self.width - 2.0 * self.free_width) / view_box_size[0],
-            (self.height - 2.0 * self.free_width) / view_box_size[1])
+            (self.width - 2.0 * self.free_width) / (self.x_max - self.x_min),
+            (self.height - 2.0 * self.free_width) / (self.y_max - self.y_min))
         return True
 
     def getPointInImage(self, point_in_world):
@@ -67,8 +133,7 @@ class Renderer(object):
         return point_in_world
 
     def updateImage(self, svg_data, line_width=1):
-        min_x, min_y, view_width, view_height = svg_data['view_box']
-        self.updateTransform([min_x, min_y], [view_width, view_height])
+        self.updateTransform(svg_data)
 
         for segment, dtype, semantic_id, instance_id in zip(
                 svg_data['segment_list'], svg_data['dtype_list'],
