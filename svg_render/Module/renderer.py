2f2fffa927ecfa9e48cc18f3a5e50f11e4f46320
chLi <565353780@qq.com>
2023-03-17 00:11:03 +0800

    add initial data loading method

--- svg_render/Module/renderer.py
+++ svg_render/Module/renderer.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import cv2
+import numpy as np
+import xml.etree.ElementTree as ET
+from copy import deepcopy
+from svgpathtools import parse_path
+
 
 class Renderer(object):
 
@@ -12,4 +18,8 @@ class Renderer(object):
         return True
 
     def renderFile(self, svg_file_path):
+        assert os.path.exists(svg_file_path)
+
+        tree = ET.parse(svg_file_path)
+        root = tree.getroot()
         return True
